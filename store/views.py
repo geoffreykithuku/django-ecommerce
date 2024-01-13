5199ed9f65a79320f9da2b4b5c48b74ce11db8c1
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-01-13 22:47:45 +0300

    design login form

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Category, Customer, Product, Order
+
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 
 def home(request):
@@ -9,3 +12,9 @@ def home(request):
 def about(request):
     products = Product.objects.all()
     return render(request, 'about.html', {'products': products})
+
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    return render(request, 'logout.html')
