2ea3d640be42f4fc3d3a1d751e3e4c628ff82506
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-01-27 15:33:48 +0300

    add product to cart

--- cart/cart.py
+++ cart/cart.py
@@ -8,3 +8,13 @@ class Cart():
         if 'session_key' not in request.session:
             cart = self.session['session_key'] = {}
         self.cart = cart
+
+    def add(self, product):
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            pass 
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
