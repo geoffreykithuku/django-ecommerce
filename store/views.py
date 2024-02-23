4863fa98ee53ddbf0fa219fdae47c9d4f177a56f
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-02-23 22:17:21 +0300

    handle update user logic

--- store/views.py
+++ store/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User 
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm
 
 # Create your views here.
 
@@ -80,4 +80,18 @@ def category_summary(request):
 
 
 def update_user(request):
-    return render(request, 'update_user.html')
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        form = UpdateUserForm(request.POST or None, instance=current_user)
+
+        if form.is_valid():
+            form.save()
+
+            login(request, current_user)
+            messages.success(request, "User has been updated successfuly")
+            return redirect('home')
+        return render(request, 'update_user.html', {'form': form})
+    else:
+        messages.error(request, "You need to be logged in to update your profile")
+        return redirect('login')
+    
